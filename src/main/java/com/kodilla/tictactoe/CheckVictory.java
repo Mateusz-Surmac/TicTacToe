afa7dcf5d7b69e4e843305efe25a330a2c5e0e15
Mateusz Surmacewicz <mateusz.surma.ch18@gmail.com>
2023-02-19 21:46:19 +0100

    First Commit

---
+++ src/main/java/com/kodilla/tictactoe/CheckVictory.java
@@ -0,0 +1,7 @@
+package com.kodilla.tictactoe;
+
+public class CheckVictory extends Game{
+    public static void checkVictory3x3Column() {
+
+    }
+}
---
+++ src/test/java/com/kodilla/tictactoe/testTicTacToe.java
@@ -0,0 +1,2 @@
+package com.kodilla.tictactoe;public class testTicTactoe {
+}
